a18dce85cf17455306e4995da04448a7def82e3a
cdada5454 <2994262185@qq.com>
2025-02-28 02:00:48 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,10 +1,3 @@
 # Add a feed source
-echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-echo 'src-git u2af https://github.com/Zxilly/UA2F' >>feeds.conf.default
-echo 'src-git rkp-ipid https://github.com/CHN-beta/rkp-ipid' >>feeds.conf.default
-echo 'src-git luci-app-ua2f https://github.com/lucikap/luci-app-ua2f' >>feeds.conf.default
 echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
 echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
-curl -fsSL https://raw.githubusercontent.com/ywt114/diy/main/feeds > feeds.conf.default
-sed -i "s/src-git/src-git-full/g" feeds.conf.default
-sed -i "s/src-git-full-full/src-git-full/g" feeds.conf.default
